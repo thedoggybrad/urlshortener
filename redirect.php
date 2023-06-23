82460153a5f07f504a25af1935c77e6afc93f2fd
TheDoggyBrad <94173621+thedoggybrad@users.noreply.github.com>
2023-06-23 16:02:39 +0800

    Add files via upload

---
+++ redirect.php
@@ -0,0 +1,22 @@
+<?php
+// Handle shortened URL requests
+$shortCode = $_GET['code'];
+
+if (!empty($shortCode)) {
+    // Load the URLs from the XML file
+    $xml = simplexml_load_file('urls.xml');
+
+    // Find the URL with the matching short code
+    $urlNode = $xml->xpath("//url[shortCode='$shortCode']");
+
+    if (!empty($urlNode)) {
+        $originalURL = (string) $urlNode[0]->originalURL;
+        header('Location: ' . $originalURL);
+        exit;
+    }
+}
+
+// If the short code doesn't exist or the code parameter is empty, redirect to index.html
+header('Location: index.html');
+exit;
+?>
---
+++ shorten.php
@@ -0,0 +1,53 @@
+<?php
+// Check if a URL is valid
+function isValidURL($url) {
+    return filter_var($url, FILTER_VALIDATE_URL) !== false;
+}
+
+// Generate a short code
+function generateShortCode() {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $length = 6;
+    $shortCode = '';
+
+    for ($i = 0; $i < $length; $i++) {
+        $randomIndex = mt_rand(0, strlen($characters) - 1);
+        $shortCode .= $characters[$randomIndex];
+    }
+
+    return $shortCode;
+}
+
+// Handle the form submission
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $url = $_POST['url'];
+
+    if (isValidURL($url)) {
+        // Generate a short code
+        $shortCode = generateShortCode();
+
+        // Load the existing URLs from the XML file
+        $xml = simplexml_load_file('urls.xml');
+
+        // Create a new URL node
+        $urlNode = $xml->addChild('url');
+        $urlNode->addChild('shortCode', $shortCode);
+        $urlNode->addChild('originalURL', $url);
+
+        // Save the updated XML back to the file with formatted output
+        $dom = new DOMDocument('1.0');
+        $dom->preserveWhiteSpace = false;
+        $dom->formatOutput = true;
+        $dom->loadXML($xml->asXML());
+        $dom->save('urls.xml');
+
+        // Return the shortened URL as response
+        $shortURL = 'https://url.x10.bz/' . $shortCode;
+        echo 'Shortened URL: <a href="' . $shortURL . '">' . $shortURL . '</a>';
+        exit;
+    } else {
+        echo 'Invalid URL';
+        exit;
+    }
+}
+?>
