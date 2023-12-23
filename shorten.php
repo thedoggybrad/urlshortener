75ad43fe6258a678b91797674d2e42fd06839f0c
TheDoggyBrad Software Lab <94173621+thedoggybrad@users.noreply.github.com>
2023-12-23 13:20:07 +0800

    PHP 8.1 compliance update

--- shorten.php
+++ shorten.php
@@ -1,13 +1,12 @@
 <?php
 // Check if a URL is valid
-function isValidURL($url) {
+function isValidURL(string $url): bool {
     return filter_var($url, FILTER_VALIDATE_URL) !== false;
 }
 
 // Generate a short code
-function generateShortCode() {
+function generateShortCode(int $length = 6): string {
     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $length = 6;
     $shortCode = '';
 
     for ($i = 0; $i < $length; $i++) {
@@ -19,7 +18,7 @@ function generateShortCode() {
 }
 
 // Check if a short code already exists
-function isDuplicateShortCode($xml, $shortCode) {
+function isDuplicateShortCode(SimpleXMLElement $xml, string $shortCode): bool {
     foreach ($xml->url as $urlNode) {
         if ((string) $urlNode->shortCode === $shortCode) {
             return true;
@@ -32,6 +31,15 @@ function isDuplicateShortCode($xml, $shortCode) {
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $url = $_POST['url'];
 
+    // Add "http://" or "https://" if not provided and check for a valid TLD
+    if (!preg_match('#^https?://#', $url)) {
+        if (strpos($url, '.') === false) {
+            echo 'Invalid URL';
+            exit;
+        }
+        $url = 'https://' . $url;
+    }
+
     if (isValidURL($url)) {
         // Generate a unique short code
         $xml = simplexml_load_file('urls.xml');
@@ -41,22 +49,30 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $shortCode = generateShortCode();
         }
 
-        // Create a new URL node
-        $urlNode = $xml->addChild('url');
-        $urlNode->addChild('shortCode', $shortCode);
-        $urlNode->addChild('originalURL', $url);
+        try {
+            // Create a new URL node
+            $urlNode = $xml->addChild('url');
+            $urlNode->addChild('shortCode', $shortCode);
+            $urlNode->addChild('originalURL', $url);
 
-        // Save the updated XML back to the file with formatted output
-        $dom = new DOMDocument('1.0');
-        $dom->preserveWhiteSpace = false;
-        $dom->formatOutput = true;
-        $dom->loadXML($xml->asXML());
-        $dom->save('urls.xml');
+            // Save the updated XML back to the file with formatted output
+            $dom = new DOMDocument('1.0');
+            $dom->preserveWhiteSpace = false;
+            $dom->formatOutput = true;
+            $dom->loadXML($xml->asXML());
+            $dom->save('urls.xml');
 
-        // Return the shortened URL as response
-        $shortURL = 'https://' . $_SERVER['HTTP_HOST'] . '/' . $shortCode;
-        echo 'Shortened URL: <a href="' . $shortURL . '">' . $shortURL . '</a>';
-        exit;
+            // Return the shortened URL as response
+            $shortURL = 'https://' . $_SERVER['HTTP_HOST'] . '/' . $shortCode;
+            echo '<a href="' . $shortURL . '">' . $shortURL . '</a>';
+            exit;
+        } catch (DOMException $e) {
+            echo 'Error processing request: ' . $e->getMessage();
+            exit;
+        } catch (Exception $e) {
+            echo 'Error processing request: ' . $e->getMessage();
+            exit;
+        }
     } else {
         echo 'Invalid URL';
         exit;
